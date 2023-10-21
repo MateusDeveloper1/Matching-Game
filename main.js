f1d27427e1102546051c3fd29b94ecb59bb52bf8
Mateus Silva <mateusilva767@gmail.com>
2023-10-21 10:51:59 -0300

    :tada: Create CardGame component

---
+++ main.js
@@ -0,0 +1,10 @@
+import CardGame from "./src/components/CardGame";
+import "./src/styles/settings/colors.css";
+import "./src/styles/generic/reset.css"
+import "./src/styles/elements/base.css"
+
+const $root = document.querySelector("#root");
+
+const $htmlCardGame = CardGame();
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
