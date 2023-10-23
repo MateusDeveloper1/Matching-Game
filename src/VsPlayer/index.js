59cdd66ce05b9a482a409d3326cb7a1656cc3ef6
Mateus Silva <mateusilva767@gmail.com>
2023-10-23 10:19:53 -0300

    :construction: Create the VsPlayer component without CSS

---
+++ src/VsPlayer/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+function VSPlayer(content = "vs") {
+  return `
+    <span class="vs-player">${content}</span>
+  `;
+}
+
+export default VSPlayer;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import PlayerName from "../../components/PlayerName";
+import VSPlayer from "../../VsPlayer";
 
 function ScoreBoard() {
   return `
     <header class="score-board">
       ${PlayerName("Player1")}
+      ${VSPlayer()}
       ${PlayerName("Player2")}
     </header>
   `;
