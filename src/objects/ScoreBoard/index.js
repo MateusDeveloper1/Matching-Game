0395fae2b447a1c1bb74ce331c3713972d7432ec
Mateus Silva <mateusilva767@gmail.com>
2023-10-23 16:02:13 -0300

    :construction: Create ArrowDown component without CSS

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -2,10 +2,12 @@ import "./styles.css";
 import PlayerName from "../../components/PlayerName";
 import VSPlayer from "../../VsPlayer";
 import PlayerScore from "../../components/PlayerScore";
+import ArrowDown from "../../components/ArrowDown";
 
 function ScoreBoard() {
   return `
     <header class="score-board">
+      ${ArrowDown()}
       ${PlayerName("Player1")}
       ${PlayerScore(1)}
       ${VSPlayer()}
