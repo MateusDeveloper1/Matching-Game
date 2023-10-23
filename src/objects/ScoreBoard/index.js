3a900ccb5997abc19c4bb73443ef8815c781a826
Mateus Silva <mateusilva767@gmail.com>
2023-10-23 12:07:29 -0300

    :construction: Create HTML of the PlayerScore component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,15 @@
 import "./styles.css";
 import PlayerName from "../../components/PlayerName";
 import VSPlayer from "../../VsPlayer";
+import PlayerScore from "../../components/PlayerScore";
 
 function ScoreBoard() {
   return `
     <header class="score-board">
       ${PlayerName("Player1")}
+      ${PlayerScore(1)}
       ${VSPlayer()}
+      ${PlayerScore()}
       ${PlayerName("Player2")}
     </header>
   `;
